de81cced5853c16b83054fa39d7d62aa148e3c3d
Saikiran <psk.knr@gmail.com>
2025-05-09 23:42:34 +0530

    Email Service in prod fix

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -14,19 +14,17 @@ export async function POST(req) {
 
     // Create transporter using GoDaddy SMTP
     const transporter = nodemailer.createTransport({
-      host: "smtpout.secureserver.net", //process.env.SMTP_HOST, // e.g., "smtpout.secureserver.net"
+      host: process.env.SMTP_HOST, // e.g., "smtpout.secureserver.net"
       port: 465,
       secure: true,
       auth: {
-        user: "info@asquaredesignstudio.co.in", //process.env.SMTP_USER, // full email like contact@yourdomain.com
-        pass: "Kiran1812!", //process.env.SMTP_PASS,
+        user: process.env.SMTP_USER, // full email like contact@yourdomain.com
+        pass: process.env.SMTP_PASS,
       },
     });
-    // console.log("env variable1",process.env.SMTP_USER )
-    // console.log("env variable",process.env.SMTP_HOST )
     const mailOptions = {
-      from: "info@asquaredesignstudio.co.in", //process.env.SMTP_USER,
-      to: "info@asquaredesignstudio.co.in", // process.env.SMTP_RECEIVER, // your destination email, e.g., your Gmail or same domain
+      from: process.env.SMTP_USER,
+      to:   process.env.SMTP_RECEIVER, // your destination email, e.g., your Gmail or same domain
       subject: `New Contact Form Submission from ${name}`,
       html: `
         <h2>New Contact Form Submission</h2>
